8a1b23b46e0757facd326c6606fb1f0d897ffc43
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-26 21:21:40 +0000

    chaged array values to 32 in 2nd_func

--- 2nd_func.c
+++ 2nd_func.c
@@ -47,7 +47,7 @@ int print_unsign(va_list h)
 int print_octal(va_list h)
 {
 	unsigned int num = va_arg(h, unsigned int);
-	int total, oct[12], i = 0;
+	int total, oct[32], i = 0;
 
 	while (num > 0)
 	{
@@ -70,7 +70,7 @@ int print_octal(va_list h)
 int print_heXa(va_list h)
 {
 	unsigned int num = va_arg(h, unsigned int);
-	int total, heX[12], i = 0;
+	int total, heX[32], i = 0;
 
 	while (num > 0)
 	{
@@ -97,7 +97,7 @@ int print_heXa(va_list h)
 int print_hexa(va_list h)
 {
 	unsigned int num = va_arg(h, unsigned int);
-	int total, hex[12], i = 0;
+	int total, hex[32], i = 0;
 
 	while (num > 0)
 	{
