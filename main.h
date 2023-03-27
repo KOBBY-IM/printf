2084d6f58ae07ebd33b456457b557e4b3c65d228
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-28 00:53:47 +0100

    Creation of flag_char function to check flag chracters, creation of spec_con function to check specifier conversions, addtion of respective prototypes in main.h

--- main.h
+++ main.h
@@ -5,12 +5,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#define SIZE 13
 
 /**
  * uil - typedef for unsigned long int
  */
 typedef unsigned long int uil;
 
+/**
+ * struct check - Holds a character and matching function call
+ * @c: character element
+ * @f: pointer to fucntion call
+ *
+ */
+typedef struct check
+{
+	char c;
+	int (*f)(va_list);
+} fmt;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
@@ -28,17 +40,7 @@ int print_p(va_list h);
 int print_S(va_list h);
 int print_rev(va_list h);
 int print_rot13(va_list h);
-
-/**
- * struct check - Holds a character and matching function call
- * @c: character element
- * @f: pointer to fucntion call
- *
- */
-typedef struct check
-{
-	char c;
-	int (*f)(va_list);
-} fmt;
+int con_spec(const char *ptr, int *a, fmt specptr[SIZE], va_list point, int m);
+int flag_char(const char *str, va_list point, int *a);
 
 #endif
